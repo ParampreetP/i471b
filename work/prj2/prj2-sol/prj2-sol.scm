241d72cba4227f4e9e61ea7e7c41554642b60c86
Parampreet Parmar <Pparmar2@binghamton.edu>
2021-03-16 02:02:14 -0400

    fixed some code proj2

--- work/prj2/prj2-sol/prj2-sol.scm
+++ work/prj2/prj2-sol/prj2-sol.scm
@@ -14,30 +14,22 @@
 ;;Given a proper-list list of proper-lists, return the sum of the
 ;;lengths of all the top-level contained lists.
 (define (sum-lengths list)
-  (cond
-((null? list)
-    0)
-((pair? (car list))
-    (+(sum-lengths (car list)) (sum-lengths(cdr list))))
-((list? (car list))
-    (+(sum-lengths (car list)) (sum-lengths(cdr list))))
-(else
-    (+ (car list) (sum-lengths (cdr list))))))
-
-;todo... wrong output for test cases like (sum-lengths '( (1 2 3) (()) (() 2 (3 4 5))))
-
-
+  (if (null? list)
+    0
+    (+ (length(car list)) (sum-lengths (cdr list)))  
+  )
+)
 ;;Evaluate polynomial with list of coefficients coeffs (highest-order
 ;;first) at x.  The computation should reflect the traditional
 ;;representation of the polynomial.
 (define (poly-eval coeffs x)
-	(if (null? coeffs)
-	0
-	(+ (expt (car coeffs) x) (poly-eval (cdr coeffs) x))
-	)
-   
+  (if (null? coeffs)
+    0
+    (let ([n (- (length coeffs) 1)] [c (car coeffs)])
+      (+ (* c (expt x n)) (poly-eval (cdr coeffs) x))
+    )  
+  )
 )
-;todo...does not work with 2nd test case
 
 ;;Evaluate polynomial with list of coefficients coeffs (highest-order
 ;;first) at x using Horner's method.
@@ -45,15 +37,15 @@
   '())  ;TODO
 
 ;;Return count of occurrences equal? to x in exp
-
-(define count-occurrences (lambda(ls x)
-                            (cond
-                              [(null? ls) 0]
-                              [(list? (car ls)) (+ (count-occurrences (car ls) x) (count-occurrences (cdr ls) x))]
-                              [(equal? x (car ls)) (+ 1 (count-occurrences (cdr ls) x))]
-                              (else (count-occurrences (cdr ls) x))
-                              )))
-;doesnt work with test case (count-occurrences '( (+ 1 2) (a (+ 1 2) 3) ) '(+ 1 2))
+(define (count-occurrences exp x)
+ (if (equal? exp x)
+    1
+    (if (pair? exp)
+      (+ (count-occurrences (car exp) x) (count-occurrences (cdr exp) x))
+      0
+    )
+  )
+)
 
 ;;Return result of evaluating arith expression over Scheme numbers
 ;;with fully parenthesized prefix binary operators 'add, 'sub, 'mul
@@ -75,16 +67,13 @@
 ;;Cannot use recursion, can use one or more of `map`, `foldl`, or `foldr`.
 (define (mul-list-2 list x)
 	(map (lambda (n)
-		(* n x))
-			list)
-)
+		(* n x)) list))
 
 ;;Given a proper-list list of proper-lists, return the sum of the
 ;;lengths of all the contained lists.  Cannot use recursion, can use
 ;;one or more of `map`, `foldl`, or `foldr`.
 (define (sum-lengths-2 list)
-  	(foldl (lambda (a b) (+ a b)) 0 list))
+  	(foldl (lambda (a b) (+ b (length a))) 0 list))
 
-; does not work first test case example
 
 		     

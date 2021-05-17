fa29061948eb2cc4a77406b0d38c1e67d4f89d4f
Parampreet Parmar <Pparmar2@binghamton.edu>
2021-05-17 14:47:05 -0400

    Submit proj5

---
+++ submit/prj5-sol/fn_server.erl
@@ -0,0 +1,35 @@
+-module(fn_server).
+
+% the function started by spawn needs to be exported, else erlang
+% seems to remove it.  It is easiest to simply export all functions.
+-compile(export_all).
+
+
+% start computational server which returns Fn(...Args) for
+% each client request of the form { ClientPid, Args }
+% where Args is a list of the arguments.
+start(Fn) -> 
+	spawn(fn_server, server, [Fn]).
+
+
+% stop fn server with PID ServerPid.
+stop(ServerPid) -> 
+	ServerPid ! stop.
+
+% return next random number from server at ServerPid
+compute(ServerPid, Args) ->
+	ServerPid ! { self(), Args},
+	receive
+		{ _, Result } -> Result
+	end.
+
+
+server(Fn) ->
+	receive
+		{ ClientPid, Args } ->
+			Result = apply(Fn, Args),
+			ClientPid ! { self(), Result },
+			server(Fn);
+		stop ->
+			true
+	end.
